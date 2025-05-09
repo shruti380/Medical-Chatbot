6c1d5bc81f1beae971ef5ebd0fa891cbac40e57a
shruti380 <shrutigupta2987@gmail.com>
2025-05-09 20:19:26 +0530

    updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy Ahmed',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Shruti Gupta',
+    author_email= 'shrutigupta.sab@gmail.com',
     packages= find_packages(),
     install_requires = []
 
